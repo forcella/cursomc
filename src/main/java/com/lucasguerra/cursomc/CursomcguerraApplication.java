06e6903cb50ade1ed05ed28095e801ed7f2d14d2
Lucas Guerra <luguerra@live.com>
2018-06-20 12:38:27 -0300

    Projeto Criado

---
+++ src/main/java/com/lucasguerra/cursomc/CursomcguerraApplication.java
@@ -0,0 +1,12 @@
+package com.lucasguerra.cursomc;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursomcguerraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursomcguerraApplication.class, args);
+	}
+}
